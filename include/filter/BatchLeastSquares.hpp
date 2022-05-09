dd5cd08eb3043a964ffae1a415f2a973daf4df9e
Parker Barrett <parkerb7@vt.edu>
2022-05-09 06:25:14 -0400

    Add weighted batch least squares and create separate function to compute cost value

--- include/filter/BatchLeastSquares.hpp
+++ include/filter/BatchLeastSquares.hpp
@@ -35,10 +35,44 @@ class BatchLeastSquares {
                                           Eigen::VectorXd &xk,
                                           double &J);
 
-        
+        /* @WeightedLinearLeastSquares
+            Inputs:
+                yk: (nxm)x1 dimensional vector of measurements (stacked)
+                Hk: (nxm)xn dimensional matrix of measurement models (stacked)
+                Wk: (nxm)x(nxm) dimensional matrix of measurment weights
+            Outputs:
+                xk: nx1 dimensional estimated state vector
+                J: Double precision Batch Least Squares cost function value
+            Description:
+                Function which takes in a batch of measurements and measurement models, yk, and Hk
+                stacked column-wise and estimates the weighted linear least squares state and 
+                cost function value estimates  
+        */
+        bool WeightedLinearLeastSquares(Eigen::VectorXd &yk,
+                                        Eigen::MatrixXd &Hk,
+                                        Eigen::MatrixXd &Wk,
+                                        Eigen::VectorXd &xk,
+                                        double &J);
 
     // Private Class Members/Function
     private:
+
+        /* @ComputeCostFunction
+            Inputs:
+                yk: (nxm)x1 dimensional vector of measurements (stacked)
+                Hk: (nxm)xn dimensional matrix of measurement models (stacked)
+                xk: nx1 dimensional estimated state vector
+            Outputs:
+                J: Double precision Batch Least Squares cost function value
+            Description:
+                Function which takes in a batch of measurements and measurement models, yk and Hk
+                stacked column-wise, along with the state estimates and computes the cost function 
+                cost function value estimates  
+        */
+        bool ComputeCostFunction(Eigen::VectorXd &yk,
+                                 Eigen::MatrixXd &Hk,
+                                 Eigen::VectorXd &xk,
+                                 double &J);
 
 
 };
--- src/filter/BatchLeastSquares.cpp
+++ src/filter/BatchLeastSquares.cpp
@@ -13,11 +13,75 @@
 // Include Headers
 #include "BatchLeastSquares.hpp"
 
-// Linear/Extended Kalman Filter Prediction
+// Linear Unweighted Batch Least Squares
 bool BatchLeastSquares::UnweightedLinearLeastSquares(Eigen::VectorXd &yk,
                                                      Eigen::MatrixXd &Hk,
                                                      Eigen::VectorXd &xk,
                                                      double &J) {
+    
+    // Get Dimension of State Vector
+    int numStates = xk.size();
+    int measBatchSize = yk.size();
+
+    // Verify Vectors/Matrices have Correct Dimensions
+    if ((Hk.rows() != measBatchSize) || (Hk.cols() != numStates)) {
+        // Add Logging
+        return false;
+    }
+
+    // Compute State Estimate
+    xk = (Hk.transpose() * Hk).inverse() * Hk.transpose() * yk; 
+
+    // Compute Cost Function
+    if (!ComputeCostFunction(yk, Hk, xk, J)) {
+        // Add Logging
+        return false;
+    }
+
+    // Return Statement for Successful Prediction
+    return true;
+
+}
+
+// Linear Weighted Batch Least Squares
+bool BatchLeastSquares::WeightedLinearLeastSquares(Eigen::VectorXd &yk,
+                                                   Eigen::MatrixXd &Hk,
+                                                   Eigen::MatrixXd &Wk,
+                                                   Eigen::VectorXd &xk,
+                                                   double &J) {
+    
+    // Get Dimension of State Vector
+    int numStates = xk.size();
+    int measBatchSize = yk.size();
+
+    // Verify Vectors/Matrices have Correct Dimensions
+    if ((Hk.rows() != measBatchSize) || (Hk.cols() != numStates)) {
+        // Add Logging
+        return false;
+    } else if ((Wk.rows() != measBatchSize) || (Wk.cols() != measBatchSize)) {
+        //Add Logging
+        return false;
+    }
+
+    // Compute State Estimate
+    xk = (Hk.transpose() * Wk * Hk).inverse() * Hk.transpose() * Wk * yk; 
+
+    // Compute Cost Function
+    if (!ComputeCostFunction(yk, Hk, xk, J)) {
+        // Add Logging
+        return false;
+    }
+
+    // Return Statement for Successful Prediction
+    return true;
+
+}
+
+// Linear Weighted Batch Least Squares
+bool BatchLeastSquares::ComputeCostFunction(Eigen::VectorXd &yk,
+                                            Eigen::MatrixXd &Hk,
+                                            Eigen::VectorXd &xk,
+                                            double &J) {
     
     // Get Dimension of State Vector
     int numStates = xk.size();
@@ -36,10 +100,9 @@ bool BatchLeastSquares::UnweightedLinearLeastSquares(Eigen::VectorXd &yk,
     J = 0;
     J = 0.5 * e.dot(e);
 
-    // Compute State Estimate
-    xk = (Hk.transpose() * Hk).inverse() * Hk.transpose() * yk; 
-
     // Return Statement for Successful Prediction
     return true;
 
 }
+
+
