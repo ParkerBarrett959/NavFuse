2885d2bf1959810c93348f10519bc79bf870f9b2
Parker Barrett <parkerb7@vt.edu>
2022-06-18 21:10:29 -0400

    Added logging statements and cleanup up code

--- src/filter/KalmanFilter.cpp
+++ src/filter/KalmanFilter.cpp
@@ -27,19 +27,24 @@ bool KalmanFilter::filterPredict(Eigen::VectorXd &xk,
 
     // Verify Vectors/Matrices have Correct Dimensions
     if ((Pk.rows() != numStates) || (Pk.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterPredict] Pk has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pk.rows() << "x" << Pk.cols() << std::endl;
         return false;
     } else if ((Phik.rows() != numStates) || (Phik.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterPredict] Phik has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Phik.rows() << "x" << Phik.cols() << std::endl;
         return false;
     } else if ((Qk.rows() != numStates) || (Qk.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterPredict] Qk has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Qk.rows() << "x" << Qk.cols() << std::endl;
         return false;
     } else if (xkp1.size() != numStates) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterPredict] xkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << "1" << ", Got " << xkp1.size() << "x" << "1" << std::endl;
         return false;
     } else if ((Pkp1.rows() != numStates) || (Pkp1.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterPredict] Pkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pkp1.rows() << "x" << Pkp1.cols() << std::endl;
         return false;
     }
 
@@ -71,22 +76,28 @@ bool KalmanFilter::filterUpdate(Eigen::VectorXd &xk,
 
     // Verify Vectors/Matrices have Correct Dimensions
     if ((Pk.rows() != numStates) || (Pk.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] Pk has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pk.rows() << "x" << Pk.cols() << std::endl;
         return false;
     } else if ((H.rows() != numMeasDimensions) || (H.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] H has incorrect dimensions: Expected " << 
+                numMeasDimensions << "x" << numStates << ", Got " << H.rows() << "x" << H.cols() << std::endl;
         return false;
     } else if ((Rk.rows() != numMeasDimensions) || (Rk.cols() != numMeasDimensions)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] Rk has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Rk.rows() << "x" << Rk.cols() << std::endl;
         return false;
     } else if ((K.rows() != numStates) || (K.cols() != numMeasDimensions)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] K has incorrect dimensions: Expected " << 
+                numStates << "x" << numMeasDimensions << ", Got " << K.rows() << "x" << K.cols() << std::endl;
         return false;
     } else if (xkp1.size() != numStates) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] xkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << "1" << ", Got " << xkp1.size() << "x" << "1" << std::endl;
         return false;
     } else if ((Pkp1.rows() != numStates) || (Pkp1.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUpdate] Pkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pkp1.rows() << "x" << Pkp1.cols() << std::endl;
         return false;
     }
 
@@ -118,13 +129,16 @@ bool KalmanFilter::filterUkfPredict(Eigen::VectorXd &Wi,
 
     // Verify Vectors/Matrices have Correct Dimensions
     if ((Pkp1.rows() != numStates) || (Pkp1.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfPredict] Pkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pkp1.rows() << "x" << Pkp1.cols() << std::endl;
         return false;
     } else if (Wi.size() != (2*numStates + 1)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfPredict] Wi has incorrect dimensions: Expected " << 
+                2*numStates + 1 << "x" << "1" << ", Got " << Wi.size() << "x" << "1" << std::endl;
         return false;
     } else if ((yi.rows() != numStates) || (yi.cols() != (2*numStates + 1))) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfPredict] yi has incorrect dimensions: Expected " << 
+                numStates << "x" << 2*numStates+1 << ", Got " << yi.rows() << "x" << yi.cols() << std::endl;
         return false;
     }
 
@@ -163,22 +177,28 @@ bool KalmanFilter::filterUkfUpdate(Eigen::VectorXd &xk,
 
     // Verify Vectors/Matrices have Correct Dimensions
     if ((Pk.rows() != numStates) || (Pk.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] Pk has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pk.rows() << "x" << Pk.cols() << std::endl;
         return false;
     } else if (Wi.size() != (2*numStates + 1)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] Wi has incorrect dimensions: Expected " << 
+                2*numStates+1 << "x" << "1" << ", Got " << Wi.size() << "x" << "1" << std::endl;
         return false;
     } else if ((zi.rows() != numMeasDimensions) || (zi.cols() != (2*numStates + 1))) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] zi has incorrect dimensions: Expected " << 
+                numMeasDimensions << "x" << 2*numStates+1 << ", Got " << zi.rows() << "x" << zi.cols() << std::endl;
         return false;
     } else if ((Rk.rows() != numMeasDimensions) || (Rk.cols() != numMeasDimensions)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] Rk has incorrect dimensions: Expected " << 
+                numMeasDimensions << "x" << numMeasDimensions << ", Got " << Rk.rows() << "x" << Rk.cols() << std::endl;
         return false;
     } else if (xkp1.size() != numStates) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] xkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << "1" << ", Got " << xkp1.size() << "x" << "1" << std::endl;
         return false;
     } else if ((Pkp1.rows() != numStates) || (Pkp1.cols() != numStates)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] Pkp1 has incorrect dimensions: Expected " << 
+                numStates << "x" << numStates << ", Got " << Pkp1.rows() << "x" << Pkp1.cols() << std::endl;
         return false;
     }
 
@@ -203,7 +223,8 @@ bool KalmanFilter::filterUkfUpdate(Eigen::VectorXd &xk,
     // Compute Kalman Gain
     Eigen::MatrixXd K = T * S.inverse();
     if ((K.rows() != numStates) || (K.cols() != numMeasDimensions)) {
-        // Add Logging
+        std::cout << "[KalmanFilter::filterUkfUpdate] K has incorrect dimensions: Expected " << 
+                numStates << "x" << numMeasDimensions << ", Got " << K.rows() << "x" << K.cols() << std::endl;
         return false;
     } 
 
