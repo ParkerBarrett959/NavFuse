579f03bcd241fa9e37d2148158f3b72a0fb57cb4
Parker Barrett <parkerb7@vt.edu>
2022-06-27 06:30:28 -0400

    Added ECEF error dynamics and began NED error dynamics

--- include/inertial/errorDynamics.hpp
+++ include/inertial/errorDynamics.hpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include "NavUtils.hpp"
+#include "Gravity.hpp"
 
 // Inertial Navigation Measurement Compensator Class
 class InertialErrorDynamics {
@@ -40,8 +41,62 @@ class InertialErrorDynamics {
                                    Eigen::MatrixXd &F,
                                    Eigen::MatrixXd &G);
 
+        /* @ecefErrorDynamics
+            Inputs:
+                aE: 3x1 vector of sensed accelerations in the ECEF frame [m/s^2]
+                GE: 3x3 Gravity Gradient Matrix
+                RS2E: 3x3 rotation matrix relating the sensor frame to the ECEF frame
+                OIE_E: 3x3 matrix of cross product equivalent of angular rate of ECEF wrt inertial expressed in ECEF 
+            Outputs:
+                F: 9x9 ECEF frame error model state transition matrix
+                G: 9x9 ECEF frame error model forcing matrix
+            Description:
+                Function which computes the ECEF frame inertial navigation error dynamics.
+                The error state vector, dx, is [attitude; velocity; position], and the forcing 
+                function vector, du, is [angular rate; acceleration; gravity]. The general form 
+                of the error dynamics are: d/dt(dx) = F * dx + G * du. The error dynamics 
+                equations are defined in "Inertial Navigation Systems with Geodetic Applications", 
+                5.3.2 (Jekeli, de Gruyter).
+        */
+        bool ecefErrorDynamics(Eigen::Vector3d &aE,
+                               Eigen::Matrix3d &GE,
+                               Eigen::Matrix3d &RS2E,
+                               Eigen::Matrix3d &OIE_E,
+                               Eigen::MatrixXd &F,
+                               Eigen::MatrixXd &G);
+
+        /* @nedErrorDynamics
+            Inputs:
+                aN: 3x1 vector of sensed accelerations in the NED frame [m/s^2]
+                GN: 3x3 Gravity Gradient Matrix
+                RS2N: 3x3 rotation matrix relating the sensor frame to the NED frame 
+                lla: 3x1 vector containing latitude, longitude and altitude
+                llaDot: 3x1 vector containing time derivatives of latitude, longitude and altitude
+            Outputs:
+                F: 9x9 NED frame error model state transition matrix
+                G: 9x9 NED frame error model forcing matrix
+            Description:
+                Function which computes the NED frame inertial navigation error dynamics.
+                The error state vector, dx, is [attitude; velocity; position], and the forcing 
+                function vector, du, is [angular rate; acceleration; gravity]. The general form 
+                of the error dynamics are: d/dt(dx) = F * dx + G * du. All elements of the gravity
+                gradient matrix, expect the [3,3] element, are assumed equal to zero. The error dynamics 
+                equations are defined in "Inertial Navigation Systems with Geodetic Applications", 
+                5.3.3 (Jekeli, de Gruyter).
+        */
+        bool nedErrorDynamics(Eigen::Vector3d &aN,
+                              Eigen::Matrix3d &GN,
+                              Eigen::Matrix3d &RS2N,
+                              Eigen::Vector3d &lla,
+                              Eigen::Vector3d &llaDot,
+                              Eigen::MatrixXd &F,
+                              Eigen::MatrixXd &G);
+
     // Private Class Members/Function
     private:
+
+        // Object Instantiations
+        Gravity Gravity;
 
 };
 
--- src/inertial/errorDynamics.cpp
+++ src/inertial/errorDynamics.cpp
@@ -10,7 +10,7 @@
 // Include Headers
 #include "errorDynamics.hpp"
 
-// Set Accelerometer Errors
+// Inertial Frame Error Dynamics
 bool InertialErrorDynamics::inertialErrorDynamics(Eigen::Vector3d &aI,
                                                   Eigen::Matrix3d &GI,
                                                   Eigen::Matrix3d &RS2I,
@@ -55,6 +55,128 @@ bool InertialErrorDynamics::inertialErrorDynamics(Eigen::Vector3d &aI,
 
     // Set Jacobian of Velocity wrt Gravity Errors
     G.block<3,3>(3,6) = Eigen::Matrix3d::Identity();
+
+    // Return for Successful Inertial Error Dynamics
+    return true;
+
+}
+
+// ECEF Frame Error Dynamics
+bool InertialErrorDynamics::ecefErrorDynamics(Eigen::Vector3d &aE,
+                                              Eigen::Matrix3d &GE,
+                                              Eigen::Matrix3d &RS2E,
+                                              Eigen::Matrix3d &OEI_E,
+                                              Eigen::MatrixXd &F,
+                                              Eigen::MatrixXd &G) {
+
+    // Check for Correct Input Sizes
+    if ((F.rows() != 9) || (F.cols() != 9)) {
+        std::cout << "[InertialErrorDynamics::ecefErrorDynamics] F has incorrect dimensions: Expected " << 
+                "9x9, Got " << F.rows() << "x" << F.cols() << std::endl;
+        return false;
+    } else if ((G.rows() != 9) || (G.cols() != 9)) {
+        std::cout << "[InertialErrorDynamics::ecefErrorDynamics] G has incorrect dimensions: Expected " << 
+                "9x9, Got " << G.rows() << "x" << G.cols() << std::endl;
+        return false;
+    }
+
+    // Set F and G to Zero Matrices
+    F = Eigen::Matrix3d::Zero();
+    G = Eigen::Matrix3d::Zero();
+
+    // Set Jacobian of Attitude wrt Attitude
+    F.block<3,3>(0,0) = - OEI_E;
+ 
+    // Set Jacobian of Velocity wrt Attitude
+    NavUtils NavUtil;
+    Eigen::Matrix3d aX;
+    if (!NavUtil.skewSymmetric(aE, aX)) {
+        std::cout << "[InertialErrorDynamics::ecefErrorDynamics] Unable to set skew symmetric" << std::endl;
+        return false;
+    }
+    F.block<3,3>(3,0) = aX;
+
+    // Set Jacobean of Velocity wrt Velocity
+    F.block<3,3>(3,3) = -2.0 * OEI_E;
+
+    // Set Jacobian of Position wrt Velocity
+    F.block<3,3>(6,3) = Eigen::Matrix3d::Identity();
+
+    // Set Jacobian of Velocity wrt Position
+    F.block<3,3>(3,6) = -((OEI_E * OEI_E) - GE);
+
+    // Set Jacobian of Attitude wrt Angular Rate Errors
+    G.block<3,3>(0,0) = -RS2E;
+
+    // Set Jacobian of Velocity wrt Acceleration Errors
+    G.block<3,3>(3,3) = RS2E;
+
+    // Set Jacobian of Velocity wrt Gravity Errors
+    G.block<3,3>(3,6) = Eigen::Matrix3d::Identity();
+
+    // Return for Successful Inertial Error Dynamics
+    return true;
+
+}
+
+// NED Frame Error Dynamics
+bool InertialErrorDynamics::nedErrorDynamics(Eigen::Vector3d &aN,
+                                             Eigen::Matrix3d &GN,
+                                             Eigen::Matrix3d &RS2N,
+                                             Eigen::Vector3d &lla,
+                                             Eigen::Vector3d &llaDot,
+                                             Eigen::MatrixXd &F,
+                                             Eigen::MatrixXd &G) {
+
+    // Check for Correct Input Sizes
+    if ((F.rows() != 9) || (F.cols() != 9)) {
+        std::cout << "[InertialErrorDynamics::nedErrorDynamics] F has incorrect dimensions: Expected " << 
+                "9x9, Got " << F.rows() << "x" << F.cols() << std::endl;
+        return false;
+    } else if ((G.rows() != 9) || (G.cols() != 9)) {
+        std::cout << "[InertialErrorDynamics::nedErrorDynamics] G has incorrect dimensions: Expected " << 
+                "9x9, Got " << G.rows() << "x" << G.cols() << std::endl;
+        return false;
+    }
+
+    // Unpack Position/Rates/Accelerations
+    double lat = lla[0];
+    double lon = lla[1];
+    double h = lla[2];
+    double latDot = llaDot[0];
+    double lonDot = llaDot[1];
+    double hDot = llaDot[2];
+    double a1 = aN[0];
+    double a2 = aN[1];
+    double a3 = aN[2];
+    double G33 = GN[2][2];
+
+    // Get Earth Constants and Parameters
+    double wE = Gravity.gravityParams.wE;
+    double fE = Gravity.gravityParams.f;
+    double a = Gravity.gravityParams.a;
+
+    // Compute Useful Quantities
+    double eSq = (2.0 * fE) - std::pow(fE, 2); 
+    double slat = std::sin(lat);
+    double clat = std::cos(lat);
+    double s2lat = std::sin(2.0 * lat);
+    double c2lat = std::cos(2.0 * lat);
+    double N = a / (std::sqrt(1 - (eSq * std::pow(slat, 2))));
+    double M = (a * (1 - eSq)) / std::pow(1 - (eSq * std::pow(slat, 2)), 1.5);
+    double ep = ;
+    double RPhi = M * std::pow(1 + (0.007 * std::pow(clat, 2)), 0.5);
+    double r = RPhi + h;
+    double l1Dot = lonDot + wE;
+    double l2Dot = lonDot + (2 * wE);  
+
+    // Set Free Inertial Dynamics Matrix
+    F << 0.0, -l1Dot*slat, -latDot, 0.0, clat, 0.0, -l1Dot*slat, 0.0, 0.0,
+         l1Dot*slat, 0.0, l1Dot*clat, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+         -latDot, -l1Dot*clat, 0.0, 0.0, -slat, 0.0, -l1Dot*clat, 0.0, 0.0,
+         0.0, -a3/r, a2/r, -2.0*hDot/r, -l1Dot*s2lat, -2.0*latDot/r, -lonDot*l2Dot*c2lat, ;
+
+    // ToDo Above - Last element of line 4 is lat double dot
 
     // Return for Successful Inertial Error Dynamics
     return true;
