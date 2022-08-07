10056fc768ea5844be31022effcec11dc3ad91fe
Parker Barrett <parkerb7@vt.edu>
2022-08-07 16:10:28 -0400

    Added first real unit test and fixed CMakeLists.txt to link with nav library

--- test/BatchLeastSquaresTest.cpp
+++ test/BatchLeastSquaresTest.cpp
@@ -1,7 +1,35 @@
-// SubtractTest.cpp
-#include "gtest/gtest.h"
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                  Batch Least Squares Unit Testing                                //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  // 
+// Author:      Parker Barrett                                                                      //
+// Overview:    Batch Least Squares Class Unit Tests.                                               //
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(SubtractTest, SubtractTwoNumbers)
+// Include Statements
+#include "gtest/gtest.h"
+#include "BatchLeastSquares.hpp"
+#include <Eigen/Dense>
+
+// Unweighted Linear Least Squares: Incorrect H Dimensions
+TEST(UnweightedLinearLeastSquares, IncorrectHDimensions)
 {
-    EXPECT_EQ(5, 5);
+
+    // Create Least Squares Object
+    BatchLeastSquares bls;
+
+    // Initialize Variables
+    Eigen::MatrixXd Hk = Eigen::MatrixXd::Zero(2,5);
+    Eigen::VectorXd yk = Eigen::VectorXd::Zero(2);
+    Eigen::VectorXd xk = Eigen::VectorXd::Zero(4);
+    double J = 0.0;
+
+    // H Matrix has Incorrect Number of Columns
+    EXPECT_FALSE(bls.UnweightedLinearLeastSquares(yk, Hk, xk, J));
+
+    // H Matrix has Incorrect Number of Rows
+
+    // H Matrix has More Columns than Rows
+
 }
