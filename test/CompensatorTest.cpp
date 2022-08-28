0b6fd3a46b02627b62a7bb90d4547a4a7cd52893
Parker Barrett <parkerb7@vt.edu>
2022-08-28 18:08:52 -0400

    Added compensator unit tests

--- test/CompensatorTest.cpp
+++ test/CompensatorTest.cpp
@@ -11,6 +11,7 @@
 #include "gtest/gtest.h"
 #include "compensator.hpp"
 #include <Eigen/Dense>
+#include <string>
 
 // Set Accelerometer Errors: Incorrect Misalignment Dimensions
 TEST(SetErrors, AccelerometerErrors)
@@ -143,5 +144,73 @@ TEST(GetErrors, GyroscopeErrorValues)
     EXPECT_EQ(bgEst, bg);
     EXPECT_EQ(sfgEst, sfg);
     EXPECT_EQ(mgEst, mg);
+
+}
+
+// Compensate Accelerometer
+TEST(CompensateAccelerometer, CompensateAccelerometer)
+{
+
+    // Create Compensator Object
+    Compensator comp;
+
+    // Initialize Variables
+    Eigen::Vector3d dV(3);
+    dV << 7.1837, 9.3832, 6.2840;
+    Eigen::Vector3d baEst(3);
+    baEst << 0.00004, 00003, 0.00007;
+    Eigen::Vector3d sfaEst(3);
+    sfaEst << 1.00031, 1.00046, 1.00035;
+    Eigen::VectorXd maEst(6);
+    maEst << 0.00001, 0.00002, 0.00003, 0.00004, 0.00005, 0.00006;
+
+    // Successfully Set Accelerometer Errors 
+    if (!comp.setAccelerometerErrors(baEst, maEst, sfaEst)) {
+        return;
+    }
+
+    // Successfully Performed Accelerometer Measurement Compensation 
+    EXPECT_TRUE(comp.compensateAccelerometer(dV));
+
+    // Define Expected Solutions
+    Eigen::Vector3d dVSol(3);
+    dVSol << 7.186077, 6.386603, 6.286872;
+
+    // Check Results
+    EXPECT_TRUE(dV.isApprox(dVSol, 1e-6));
+
+}
+
+// Compensate Gyroscope
+TEST(CompensateGyroscope, CompensateGyroscope)
+{
+
+    // Create Compensator Object
+    Compensator comp;
+
+    // Initialize Variables
+    Eigen::Vector3d dTh(3);
+    dTh << 7.1837, 9.3832, 6.2840;
+    Eigen::Vector3d bgEst(3);
+    bgEst << 0.00004, 00003, 0.00007;
+    Eigen::Vector3d sfgEst(3);
+    sfgEst << 1.00031, 1.00046, 1.00035;
+    Eigen::VectorXd mgEst(6);
+    mgEst << 0.00001, 0.00002, 0.00003, 0.00004, 0.00005, 0.00006;
+
+    // Successfully Set Gyroscope Errors 
+    if (!comp.setGyroscopeErrors(bgEst, mgEst, sfgEst)) {
+        return;
+    }
+
+    // Successfully Performed Gyroscope Measurement Compensation 
+    EXPECT_TRUE(comp.compensateGyroscope(dTh));
+
+    // Define Expected Solutions
+    Eigen::Vector3d dThSol(3);
+    dThSol << 7.186077, 6.386603, 6.286872;
+
+    // Check Results
+    EXPECT_TRUE(dTh.isApprox(dThSol, 1e-6));
 
 }
