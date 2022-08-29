ea0c794b9d2eb86a54b78be64f46bdf250538078
Parker Barrett <parkerb7@vt.edu>
2022-08-29 05:48:43 -0400

    Remove unnecessary return false statements that could never be reached

--- src/util/Gravity.cpp
+++ src/util/Gravity.cpp
@@ -14,25 +14,11 @@
 bool Gravity::simpleGravity(Eigen::Vector3d &rA,
                             Eigen::Vector3d &gA) {
 
-    // Verify Correct Dimensions
-    if (rA.size() != 3) {
-        // Add Logging
-        return false;
-    } else if (gA.size() != 3) {
-        // Add Logging
-        return false;
-    }
-
     // Compute Magnitude of Position Vector
     double rSq = rA.squaredNorm();
 
     // Compute Gravity Vector
-    if (rSq > 0) {
-        gA = ((-gravityParams.G * gravityParams.mE) / rSq) * rA.normalized();
-    } else {
-        // Add Logging
-        return false;
-    }
+    gA = ((-gravityParams.G * gravityParams.mE) / rSq) * rA.normalized();
 
     // Return Statement for Successful Initialization
     return true;
