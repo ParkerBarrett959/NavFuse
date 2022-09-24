59219d6526a130549a1939721b88030a5c94ec7d
Parker Barrett <parkerb7@vt.edu>
2022-09-24 09:23:18 -0400

    Added input vectors/matrices size unit testing for fine alugnment prediction

--- test/InitializationTest.cpp
+++ test/InitializationTest.cpp
@@ -37,9 +37,86 @@ TEST(CoarseAttitudeAlignment, ComputeResult)
     RB2NSol <<     3.70087216369588,         170.240078845804,         62.914813837855,
               -0.000392825687754338,   -0.0000528803810438532,    0.000166195483280682,
                0.000001020616452337,    0.0000020412329046744,      0.0000030618493570;
-;
 
     // Check Results
     EXPECT_TRUE(RB2N.isApprox(RB2NSol, 1e-6));
+
+}
+
+// Fine Alignment Predict: Incorrect Pk Dimensions 
+TEST(FineAlignmentPredict, IncorrectPkDimensions)
+{
+
+    // Create Initialization Object
+    Initialization init;
+
+    // Initialize Variables
+    double wE = 7.292115 * 1e-5;
+    double lat = 0.7361;
+    double g = -9.798;
+    double r = 6378000.0;
+    Eigen::VectorXd xk = Eigen::VectorXd::Zero(8);
+    Eigen::MatrixXd Pk = Eigen::MatrixXd::Zero(8, 7);
+    Eigen::VectorXd xkp1 = Eigen::VectorXd::Zero(8);
+    Eigen::MatrixXd Pkp1 = Eigen::MatrixXd::Zero(8, 8);
+
+    // Failed Fine Alignment Prediction: Incorrect Pk columns
+    EXPECT_FALSE(init.fineAlignmentPredict(lat, wE, g, r, xk, Pk, xkp1, Pkp1));
+
+    // Redefine Pk
+    Pk = Eigen::MatrixXd::Zero(7, 8);
+
+    // Failed Fine Alignment Prediction: Incorrect Pk rows
+    EXPECT_FALSE(init.fineAlignmentPredict(lat, wE, g, r, xk, Pk, xkp1, Pkp1));
+
+}
+
+// Fine Alignment Predict: Incorrect xkp1 Dimensions 
+TEST(FineAlignmentPredict, Incorrectxkp1Dimensions)
+{
+
+    // Create Initialization Object
+    Initialization init;
+
+    // Initialize Variables
+    double wE = 7.292115 * 1e-5;
+    double lat = 0.7361;
+    double g = -9.798;
+    double r = 6378000.0;
+    Eigen::VectorXd xk = Eigen::VectorXd::Zero(8);
+    Eigen::MatrixXd Pk = Eigen::MatrixXd::Zero(8, 8);
+    Eigen::VectorXd xkp1 = Eigen::VectorXd::Zero(7);
+    Eigen::MatrixXd Pkp1 = Eigen::MatrixXd::Zero(8, 8);
+
+    // Failed Fine Alignment Prediction: Incorrect xkp1 columns
+    EXPECT_FALSE(init.fineAlignmentPredict(lat, wE, g, r, xk, Pk, xkp1, Pkp1));
+
+}
+
+// Fine Alignment Predict: Incorrect Pkp1 Dimensions 
+TEST(FineAlignmentPredict, IncorrectPkp1Dimensions)
+{
+
+    // Create Initialization Object
+    Initialization init;
+
+    // Initialize Variables
+    double wE = 7.292115 * 1e-5;
+    double lat = 0.7361;
+    double g = -9.798;
+    double r = 6378000.0;
+    Eigen::VectorXd xk = Eigen::VectorXd::Zero(8);
+    Eigen::MatrixXd Pk = Eigen::MatrixXd::Zero(8, 8);
+    Eigen::VectorXd xkp1 = Eigen::VectorXd::Zero(8);
+    Eigen::MatrixXd Pkp1 = Eigen::MatrixXd::Zero(8, 7);
+
+    // Failed Fine Alignment Prediction: Incorrect Pkp1 columns
+    EXPECT_FALSE(init.fineAlignmentPredict(lat, wE, g, r, xk, Pk, xkp1, Pkp1));
+
+    // Redefine Pkp1
+    Pkp1 = Eigen::MatrixXd::Zero(7, 8);
+
+    // Failed Fine Alignment Prediction: Incorrect Pkp1 rows
+    EXPECT_FALSE(init.fineAlignmentPredict(lat, wE, g, r, xk, Pk, xkp1, Pkp1));
 
 }
