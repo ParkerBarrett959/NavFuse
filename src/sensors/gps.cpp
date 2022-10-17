4ddda1e861078b7157714338e9d1ee8234915bb1
Parker Barrett <parkerb7@vt.edu>
2022-10-17 06:37:08 -0400

    Add skeleton gps sensor model class

---
+++ src/sensors/gps.cpp
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                           GPS Sensor Model                                       //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  // 
+// Author:      Parker Barrett                                                                      //
+// Overview:    Class implementation which defines a series of GPS sensor model implementations.    //          
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Include Headers
+#include "gps.hpp"
+
+// Loosely Coupled J2K GPS Sensor Model
+bool Gps::looselyCoupledGpsJ2k(Eigen::VectorXd &x,
+                               Eigen::VectorXd &idx,
+                               bool &velValid,
+                               Eigen::MatrixXd &H) {
+
+    // Insert Implementation
+    
+    // Successful Return
+    return true;
+
+}
