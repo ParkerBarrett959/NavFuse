f3f34e5cd0b22ad85579331fbf6918733366ee99
Parker Barrett <parkerb7@vt.edu>
2022-11-10 06:34:19 -0500

    Add class for commonly used navigation data types

---
+++ include/util/dataTypes.hpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Custom Navigation Data Types                                //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  // 
+// Author:      Parker Barrett                                                                      //
+// Overview:    Custom navigation data type definitions.                                                    //           
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Include Headers
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <Eigen/Dense>
+
+// Data Types Class
+class DataTypes {
+
+    // Public Class Members
+    public:
+
+        // IMU Calibration Data
+        struct {
+            Eigen::Vector3d ba;         // Accelerometer Bias [micro-g's]
+            Eigen::Vector3d sfa;        // Accelerometer Scale Factor Error [ppm]
+            Eigen::VectorXd ma;         // Accelerometer Misalignment Coefficients [6x1]
+            Eigen::Vector3d bg;         // Gyroscope Bias [deg/hr]
+            Eigen::Vector3d sfg;        // Gyroscope Scale Factor Error [ppm]
+            Eigen::VectorXd mg;         // Gyroscope Misalignment Coefficients [6x1]
+        } imuCalibrationData_t;
+
+    // Private Class Members/Function
+    private:
+
+};
