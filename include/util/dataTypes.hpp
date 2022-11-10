6b1ab669b93cde4bc34bd00b2deac25eb5f6c1f2
Parker Barrett <parkerb7@vt.edu>
2022-11-10 06:51:58 -0500

    Adjusted structure

--- include/util/dataTypes.hpp
+++ include/util/dataTypes.hpp
@@ -15,11 +15,11 @@
 #include <Eigen/Dense>
 
 // IMU Calibration Data
-struct {
+struct imuCalibrationData_t {
     Eigen::Vector3d ba;         // Accelerometer Bias [micro-g's]
     Eigen::Vector3d sfa;        // Accelerometer Scale Factor Error [ppm]
     Eigen::VectorXd ma;         // Accelerometer Misalignment Coefficients [6x1]
     Eigen::Vector3d bg;         // Gyroscope Bias [deg/hr]
     Eigen::Vector3d sfg;        // Gyroscope Scale Factor Error [ppm]
     Eigen::VectorXd mg;         // Gyroscope Misalignment Coefficients [6x1]
- } imuCalibrationData_t;
+ };
