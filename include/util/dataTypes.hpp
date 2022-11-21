151bec733c5ef1992e426aece30efd615d5ea2f6
Parker Barrett <parkerb7@vt.edu>
2022-11-21 05:42:20 -0500

    Add imu and gps simulated sensor model error parameter data types

--- include/util/dataTypes.hpp
+++ include/util/dataTypes.hpp
@@ -29,3 +29,34 @@ struct imuCalibrationData_t {
     Eigen::Vector3d sfg;        // Gyroscope Scale Factor Error [ppm]
     Eigen::VectorXd mg;         // Gyroscope Misalignment Coefficients [6x1]
 };
+
+// IMU Sensor Simulator Data
+struct imuSensorSimData_t {
+    bool useImu;
+    double rate;                        // Data Rate [Hz]
+    double accel_bias_repeatability;    // RMS Accel Bias Repeatability [micro-g]
+    double accel_bias_instability;      // RMS Accel Bias Instability [micro-g]
+    double accel_sf;                    // Accel SF 1-Sigma [ppm]
+    double accel_mis;                   // AMIS 1-Sigma [micro-rad]
+    double accel_vrw;                   // Velocity Random Walk [m/s/sqrt(hr)]
+    double gyro_bias_repeatability;     // RMS Gyro Bias Repeatability [deg/hr]
+    double gyro_bias_instability;       // RMS Gyro Bias Instability [deg/hr]
+    double gyro_sf;                     // Gyro SF 1-Sigma [ppm]
+    double gyro_mis;                    // GMIS 1-Sigma [micro-rad]
+    double gyro_arw;                    // Angle Random Walk [deg/sqrt(hr)]
+};
+
+// Loosely Coupled GPS Sensor Simulator Data
+struct looselyCoupledGpsSensorSimData_t {
+    bool useLooselyCoupledGps;
+    double rate;                    // Data Rate [Hz]
+    double level_pos_sigma;         // Level (North/East) Position Error 1-Sigma [m]
+    double vertical_pos_sigma;      // Vertical (Down) Position Error 1-Sigma [m]
+};
+
+// Sensor Simulator Data
+struct sensorSimData_t {
+    imuSensorSimData_t imu;                   // Sensor Simulator IMU Model
+    looselyCoupledGpsSensorSimData_t gps;     // Sensor Simulator Loosely Coupled GPS Model
+    std::string trajectoryFile;               // Path to Trajectory CSV File
+};
